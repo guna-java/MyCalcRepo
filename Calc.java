bb301fc27b7f44fe45bb3512ed6d02d00364a73a
guna-java <72907641+guna-java@users.noreply.github.com>
2020-10-20 11:18:19 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -5,11 +5,17 @@ class Calc{
   public static int sub(int a,int b){
   return a-b;
   }
+  public static int prod(int a,int b){
+  return a*b;
+  }
   public static void main(String [] args){
     int c=add(10,20);
     
     System.out.println(c);
     System.out.println(sub(50,40));
+     System.out.println(prod(50,40));
+    
+  }
     
   }
 }
